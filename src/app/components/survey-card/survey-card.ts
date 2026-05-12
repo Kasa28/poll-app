9bcf6d8661a7d0a83cb9eeb129abc8925124ce21
Kasa28 <agliullinaksenija@gmail.com>
2026-05-12 19:00:00 +0200

    added new components

--- src/app/components/survey-card/survey-card.ts
+++ src/app/components/survey-card/survey-card.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-survey-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './survey-card.html',
   styleUrl: './survey-card.scss',
 })
 export class SurveyCard {
+  @Input() id = '';
   @Input() category = '';
   @Input() title = '';
   @Input() ending = '';
